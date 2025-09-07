a7461ed9f059339228ba8724a0fc95853944668f
Roger Villarroel <roger.villarroel@alumnos.ucn.cl>
2025-09-07 16:57:23 -0300

    Mover implementaciones de LinkedList.cpp a LinkedList.h (header-only) para evitar errores de templates y simplificar todo

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -1,22 +1,137 @@
-#ifndef TALLERES_ESTRUCTURAS_DE_DATOS21_LINKEDLIST_H
-#define TALLERES_ESTRUCTURAS_DE_DATOS21_LINKEDLIST_H
+#ifndef TALLER1ESTRUCTURASDEDATOS_INSCRIPCION_H
+#define TALLER1ESTRUCTURASDEDATOS_INSCRIPCION_H
 
 #include "Nodo.h"
 #include <string>
 
 template <typename T>
 class LinkedList {
-    Nodo<T> *cabeza;
+    Nodo<T>* cabeza;
 
 public:
     LinkedList();
+    ~LinkedList();
+
+    bool isEmpty() const;
+
     void agregarNodo(const T& objeto);
+
     bool idExiste(int idBuscado) const;
     T getObjeto(int idBuscado) const;
+    T* getObjetoPtr(int idBuscado);
+    const T* getObjetoPtr(int idBuscado) const;
+
     void mostrarInformacion(int idBuscado, const std::string& nombreBuscado) const;
-    void eliminarNodo(int id);
-    bool isEmpty() const;
-    ~LinkedList();
+    void eliminarNodo(int idBuscado);
 };
 
-#endif //TALLERES_ESTRUCTURAS_DE_DATOS21_LINKEDLIST_H
+template<typename T>
+LinkedList<T>::LinkedList() : cabeza(nullptr) {}
+
+template<typename T>
+LinkedList<T>::~LinkedList() {
+    while (cabeza) {
+        Nodo<T>* siguiente = cabeza->getSiguiente();
+        delete cabeza;
+        cabeza = siguiente;
+    }
+}
+
+template<typename T>
+bool LinkedList<T>::isEmpty() const {
+    return cabeza == nullptr;
+}
+
+template<typename T>
+void LinkedList<T>::agregarNodo(const T& objeto) {
+    auto* nuevo = new Nodo<T>();
+    nuevo->setObjeto(objeto);
+    nuevo->setSiguiente(nullptr);
+
+    if (!cabeza) {
+        cabeza = nuevo;
+        return;
+    }
+    Nodo<T>* actual = cabeza;
+    while (actual->getSiguiente()) actual = actual->getSiguiente();
+    actual->setSiguiente(nuevo);
+}
+
+template<typename T>
+bool LinkedList<T>::idExiste(int idBuscado) const {
+    const Nodo<T>* actual = cabeza;
+    while (actual) {
+        if (actual->getObjeto()->getId() == idBuscado) return true;
+        actual = actual->getSiguiente();
+    }
+    return false;
+}
+
+template<typename T>
+T LinkedList<T>::getObjeto(int idBuscado) const {
+    const Nodo<T>* actual = cabeza;
+    while (actual) {
+        if (actual->getObjeto()->getId() == idBuscado) {
+            return *(actual->getObjeto()); // copia
+        }
+        actual = actual->getSiguiente();
+    }
+    return T();
+}
+
+template<typename T>
+T* LinkedList<T>::getObjetoPtr(int idBuscado) {
+    Nodo<T>* actual = cabeza;
+    while (actual) {
+        T* obj = actual->getObjeto();
+        if (obj && obj->getId() == idBuscado) return obj;
+        actual = actual->getSiguiente();
+    }
+    return nullptr;
+}
+
+template<typename T>
+const T* LinkedList<T>::getObjetoPtr(int idBuscado) const {
+    const Nodo<T>* actual = cabeza;
+    while (actual) {
+        const T* obj = actual->getObjeto();
+        if (obj && obj->getId() == idBuscado) return obj;
+        actual = actual->getSiguiente();
+    }
+    return nullptr;
+}
+
+template<typename T>
+void LinkedList<T>::mostrarInformacion(int idBuscado, const std::string& nombreBuscado) const {
+    const Nodo<T>* actual = cabeza;
+    while (actual) {
+        const T* obj = actual->getObjeto();
+        if (obj && (obj->getId() == idBuscado || obj->getNombre() == nombreBuscado)) {
+            obj->mostrarInformacion();
+        }
+        actual = actual->getSiguiente();
+    }
+}
+
+template<typename T>
+void LinkedList<T>::eliminarNodo(int idBuscado) {
+    Nodo<T>* ant = nullptr;
+    Nodo<T>* actual = cabeza;
+
+    while (actual) {
+        T* obj = actual->getObjeto();
+        if (obj && obj->getId() == idBuscado) {
+            if (!ant) {
+                cabeza = actual->getSiguiente();
+            } else {
+                ant->setSiguiente(actual->getSiguiente());
+            }
+            delete actual;
+            return;
+        }
+        ant = actual;
+        actual = actual->getSiguiente();
+    }
+}
+
+#endif // TALLER1ESTRUCTURASDEDATOS_INSCRIPCION_H
