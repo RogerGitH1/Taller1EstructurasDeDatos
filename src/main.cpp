fc8de56574c5b1b974ece17d487887c69c76e5e0
Roger Villarroel <roger.villarroel@alumnos.ucn.cl>
2025-09-07 16:16:20 -0300

    Implementacion Opcion 2 Menu Principal Manejo Curso -crearCurso() -buscarCurso() -eliminarCurso()

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 #include <limits>
-#include <string>
-
 #include "Alumno.h"
+#include "Curso.h"
 #include "LinkedList.h"
 
-
 using namespace std;
-LinkedList linkedList;
+
+LinkedList<Alumno> alumnos;
+LinkedList<Curso> cursos;
 
 string leerString(string texto){
     cout << texto;
@@ -37,9 +37,9 @@ void crearAlumno(){
     string carrera = leerString("Ingrese Carrera: ");
     string fechaIngreso = leerString("Ingrese Fecha de ingreso (dia/mes/año): ");
 
-    if (!linkedList.idExiste(id)) {
+    if (!alumnos.idExiste(id)) {
         Alumno alumno(id,nombre,apellido,carrera,fechaIngreso);
-        linkedList.agregarNodo(alumno);
+        alumnos.agregarNodo(alumno);
         cout << "Alumno agregado correctamente " << endl;
         return;
     }
@@ -49,9 +49,9 @@ void crearAlumno(){
 void buscarAlumno() {
     cout << "BUSCAR ALUMNO" << endl;
     int id = leerInt("Ingrese Id: ");
-    Alumno alumno = linkedList.getAlumno(id);
+    Alumno alumno = alumnos.getObjeto(id);
     if (alumno.getId() != -1) {
-        linkedList.mostrarInformacion(alumno.getId(), alumno.getNombre());
+        alumnos.mostrarInformacion(alumno.getId(), alumno.getNombre());
         return;
     }
     cout << "Id: " << id << " no encontrado " << endl;
@@ -60,13 +60,14 @@ void buscarAlumno() {
 void eliminarAlumno() {
     cout << "ELIMINAR ALUMNO" << endl;
     int id = leerInt("Ingrese Id: ");
-    if (linkedList.idExiste(id)) {
-        linkedList.eliminarNodo(id);
+    if (alumnos.idExiste(id)) {
+        alumnos.eliminarNodo(id);
         cout << "El alumno con Id: " << id << " eliminado " << endl;
         return;
     }
     cout << "El id: " << id << " no existe " << endl;
 }
+
 void manejoAlumnos(){
     int opcion;
     cout << "MANEJO DE ALUMNOS" << endl;
@@ -94,6 +95,70 @@ void manejoAlumnos(){
 
 }
 
+void crearCurso(){
+    cout << "CREAR CURSO" << endl;
+    int id = leerInt("Ingrese Id del curso: ");
+    string nombre = leerString("Ingrese Nombre del curso: ");
+    int cantMaxima = leerInt("Ingrese cantidad maxima de estudiantes:  ");
+    string carrera = leerString("Ingrese Carrera del curso: ");
+    string profesor = leerString("Ingrese Profesor: ");
+
+    if (!cursos.idExiste(id)) {
+        Curso curso(id, nombre, cantMaxima, carrera, profesor);
+        cursos.agregarNodo(curso);
+        cout << "Curso agregado correctamente" << endl;
+        return;
+    }
+    cout << "El id: " << id << " ya existe" << endl;
+}
+
+void buscarCurso() {
+    cout << "BUSCAR CURSO" << endl;
+    int id = leerInt("Ingrese Id del curso: ");
+    Curso curso = cursos.getObjeto(id);
+    if (curso.getId() != -1) {
+        cursos.mostrarInformacion(curso.getId(), curso.getNombre());
+        return;
+    }
+    cout << "Id: " << id << " no encontrado" << endl;
+}
+
+void eliminarCurso() {
+    cout << "ELIMINAR CURSO" << endl;
+    int id = leerInt("Ingrese Id del curso: ");
+    if (cursos.idExiste(id)) {
+        cursos.eliminarNodo(id);
+        cout << "El curso con Id: " << id << " eliminado" << endl;
+        return;
+    }
+    cout << "El id: " << id << " no existe" << endl;
+}
+
+void manejoCursos(){
+    int opcion;
+    cout << "MANEJO DE CURSOS" << endl;
+    do{
+        cout << "0. Volver al menu Principal" << endl;
+        cout << "1. Crear Curso" << endl;
+        cout << "2. Buscar un curso por ID o nombre y listar su información" << endl;
+        cout << "3. Eliminar un curso del sistema utilizando su ID" << endl;
+        opcion = leerInt("Ingrese una opción: ");
+        cout << endl;
+
+        switch (opcion){
+            case 1:
+                crearCurso();
+                break;
+            case 2:
+                buscarCurso();
+                break;
+            case 3:
+                eliminarCurso();
+                break;
+        }
+    } while (opcion != 0);
+}
+
 int main(){
     int opcion;
     do{
@@ -112,9 +177,8 @@ int main(){
         case 1:
             manejoAlumnos();
             break;
-
         case 2:
-
+            manejoCursos();
             break;
         case 3:
 
